8718bc10ba1153b07445dc4906c78c9dd8a20309
SURYA KRISHNA MOORTHY <ayrus2289@gmail.com>
2021-05-05 16:22:59 -0700

    add header

--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Header = () => {
-	return <h3>Task Manager</h3>;
+	return (
+		<>
+			<h3> Task Manager </h3>
+			<hr />
+		</>
+	);
 }
 
 export default Header;
