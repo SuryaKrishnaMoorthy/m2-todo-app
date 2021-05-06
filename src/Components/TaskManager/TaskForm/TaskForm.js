87df54f4487edb56b9178d77162cfca45e2c0589
SURYA KRISHNA MOORTHY <ayrus2289@gmail.com>
2021-05-06 07:48:14 -0700

    add taskform

---
+++ src/Components/TaskManager/TaskForm/TaskForm.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+
+const TaskForm = () => {
+	return (
+		<Form>
+			<FormGroup>
+				<Label for="title">Title</Label>
+				<Input type="text" id="title"></Input>
+			</FormGroup>
+			<FormGroup>
+				<Label for="description">Description </Label>
+				<Input type="textarea" id="description"></Input>
+			</FormGroup>
+			<Button>Create</Button>
+		</Form>
+	)
+}
+
+export default TaskForm;
