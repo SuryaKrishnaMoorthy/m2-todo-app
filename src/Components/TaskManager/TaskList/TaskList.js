5cdc7b468a93c7e4720b61467d69109a0e993867
SURYA KRISHNA MOORTHY <ayrus2289@gmail.com>
2021-05-11 18:39:56 -0700

    add class task to model

--- src/Components/TaskManager/TaskList/TaskList.js
+++ src/Components/TaskManager/TaskList/TaskList.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { ListGroup } from 'reactstrap';
+
 import TaskItem from './TaskItem/TaskItem';
 
 const TaskList = () => {
-	const tasks = ["Learn React", "Learn Algorithms", "Learn Node",]
+	const tasks = ["Learn React", "Learn Algorithms", "Learn Node"]
 }
